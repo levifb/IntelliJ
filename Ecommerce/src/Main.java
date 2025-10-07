1c058808115c35573d913de7cf649fbd7ec2eef7
levifb <levi.fb.123@gmail.com>
2025-10-07 17:18:01 -0300

    terceira versão

--- Ecommerce/src/Main.java
+++ Ecommerce/src/Main.java
@@ -1,10 +1,10 @@
-
 void main() {
 
   IO.println(String.format("Seja bem vindo, segunda versão"));
 
   for (int i = 1; i <= 5; i++){
       IO.println("i = " + 2*i);
+      IO.println("Testando o git");
 
   }
 
